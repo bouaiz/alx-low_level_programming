6e6d643bd6b94b28f0b04d095050beea6f24590d
bouaiz <bouaiz.anas@gmail.com>
2023-02-16 14:57:51 -0800

    fourth solved i guess

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+
+Copy code
 /**
  * main - Entry point
  *
- * Description: Prints "Programming is like building a multilingual puzzle"
- * 		followed by new line
+ * Description: Prints "Programming is like building a multilingual puzzle",
+ *              followed by a new line
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
 }
