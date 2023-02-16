7f994ebe859e9caf0cd21c4e646e2a8574c5dd09
bouaiz <bouaiz.anas@gmail.com>
2023-02-16 14:58:42 -0800

    sorry for mistake

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,6 +10,6 @@ Copy code
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
