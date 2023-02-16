e8765f94d59f39cfc4cd141b25900c28d158693d
bouaiz <bouaiz.anas@gmail.com>
2023-02-16 14:52:19 -0800

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Description: Prints "wth proper grammar, but the outcome is a piece of art,"
+ *		followed by new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  printf("wth proper grammar, but the outcome is a piece of art,");
+  return 0;
+}
